584a471dd5ad984d4a61abb34bf05c8ab5443a83
jdcarvajal <jdcc1308@gmail.com>
2024-04-12 11:24:05 -0500

    feat: generic generarAlertas function

--- placaMivUnisalud/lib/alertasMIV/alertasMIV.cpp
+++ placaMivUnisalud/lib/alertasMIV/alertasMIV.cpp
@@ -2,7 +2,13 @@
 #include "alertasMIV.h"
 // ****************************************************************************************
 void generarAlertas(){
-    boolean superaUmbralTemp, superaUmbralHum;
-    (promedioTemperatura > 27.7) ? superaUmbralTemp = true : superaUmbralTemp = false;
-    (promedioHumedad > 63.0) ? superaUmbralHum = true : superaUmbralHum = false;
+    boolean entraEnRangoUmbralTemp, entraEnRangoUmbralHum;
+    (promedioTemperatura > 28.0) ? entraEnRangoUmbralTemp = true : entraEnRangoUmbralTemp = false;
+    (promedioHumedad > 65.0) ? entraEnRangoUmbralHum = true : entraEnRangoUmbralHum = false;
+    if(entraEnRangoUmbralTemp){
+        enviarMensaje("⚠ Entrando a rango del umbral de temperatura ⚠ Temperatura [°C] = " + String(promedioTemperatura));
+    }
+    if(entraEnRangoUmbralHum){
+        enviarMensaje("⚠ Entrando a rango del umbral de humedad ⚠ Humedad [%] = " + String(promedioHumedad));
+    }
 }
