e6e9e3ffcf388252704a440879fa8b68c081a12d
Juan David Carvajal Cucuñame <jdcc1308@gmail.com>
2024-05-14 11:57:09 -0500

    update controladorInicioSesion.php

--- aplicacionWebMivUnisalud/app/controllers/controladorInicioSesion.php
+++ aplicacionWebMivUnisalud/app/controllers/controladorInicioSesion.php
@@ -11,7 +11,7 @@
         "<script>
             confirm('Datos incorrectos')
             open('../../index.php ')
-            close('loguear.php')
+            close('/app/controllers/controladorInicioSesion.php')
         </script>";
     }
 
